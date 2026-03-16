bc1ef1f0f4fc9a5699f8142e800d3cef5cfa67d5
swayamjaiswal7 <swayam5566@gmail.com>
2026-03-16 21:04:11 +0530

    Updated Models

--- src/app.py
+++ src/app.py
@@ -13,18 +13,29 @@ st.set_page_config(
     layout="wide"
 )
 BASE_DIR = Path(__file__).resolve().parent.parent
-MODEL_PATH = BASE_DIR / "models" / "pipeline_xgb.pkl"
+MODEL_DIR = BASE_DIR / "models" 
 
+st.sidebar.header("Model Selection")
+
+model_choice = st.sidebar.selectbox("Choose Model",("Gradient Boosting",
+                                    "Random Forest","Decision Tree")
+                                    )
 @st.cache_resource
-def load_model():
-    return joblib.load(MODEL_PATH)
+def load_model(model_name):
+    model_paths = {
+        "Gradient Boosting": MODEL_DIR / "gb_pipeline.pkl",
+        "Random Forest": MODEL_DIR / "rf-pipeline.pkl",
+        "Decision Tree": MODEL_DIR / "decision_tree.pkl"
+    }
+    return joblib.load(model_paths[model_name])
 
-model = load_model()
+model = load_model(model_choice)
+st.info(f"Currently Using **{model_choice} Model** ")
 st.markdown(
 """
 # 📊 Bank Marketing Deposit Prediction
 ### Machine Learning Dashboard
-:blue-background[Predict whether a customer will subscribe to a **term deposit** using an XGBoost model.
+:blue-background[Predict whether a customer will subscribe to a **term deposit** using an Gradient Boosting model.
 ]
 """
 )
@@ -35,7 +46,7 @@ age = st.sidebar.slider("Age",18,90,40)
 
 job = st.sidebar.selectbox(
     "Job",
-    ['admin','technician','services','management','retired','blue-collar','student','entrepreneur']
+    ['admin.','technician','services','management','retired','blue-collar','student','entrepreneur']
 )
 
 marital = st.sidebar.selectbox(
@@ -72,7 +83,6 @@ contact = st.sidebar.selectbox(
     "Contact Type",
     ['cellular','telephone']
 )
-
 campaign = st.sidebar.slider("Number of Campaign Contacts",1,50,2)
 
 pdays = st.sidebar.slider("Days Since Last Contact",-1,500,100)
@@ -93,7 +103,7 @@ input_data = pd.DataFrame({
     'campaign':[campaign],
     'pdays':[pdays],
     'previous':[previous]
-})
+    })
 
 tab1, tab2, tab3, tab4 = st.tabs([
     "Prediction",
@@ -128,13 +138,16 @@ with tab1:
 with tab2:
 
     st.subheader("Feature Importance")
+    if model_choice == "Gradient Boosting":
+        gb_model = model.named_steps["gb_trf"]
+    elif model_choice == "Random Forest":
+        gb_model =model.named_steps["rf_trf"]
+    else:
+        gb_model =model.named_steps["dt_trf"]
+    encoding = model.named_steps["trf1"]
 
-    xgb_model = model.named_steps["xgb"]
-    encoding = model.named_steps["enc"]
-
-    feature_names = encoding.get_feature_names_out()
-
-    importances = xgb_model.feature_importances_
+    feature_names = [f.split("__")[-1] for f in encoding.get_feature_names_out()]
+    importances = gb_model.feature_importances_
 
     feat_df = pd.DataFrame({
         "feature":feature_names,
@@ -181,22 +194,31 @@ with tab3:
 
 with tab4:
 
-    st.subheader("SHAP Feature Impact")
+    st.subheader("SHAP Feature Impact for 500 customers")
 
-    encoding = model.named_steps["enc"]
-    xgb_model = model.named_steps["xgb"]
+    encoding = model.named_steps["trf1"]
+    if model_choice == "Gradient Boosting":
+        xgb_model = model.named_steps["gb_trf"]
+    elif model_choice == "Random Forest":
+        xgb_model =model.named_steps["rf_trf"]
+    else:
+        xgb_model =model.named_steps["dt_trf"]
 
     X_transformed = encoding.transform(input_data)
 
     explainer = shap.TreeExplainer(xgb_model)
+    X_sample = encoding.transform(X.sample(500))
 
-    shap_values = explainer.shap_values(X_transformed)
+    shap_values = explainer.shap_values(X_sample)
 
+  # Handle binary classification output
+    if isinstance(shap_values, list):
+        shap_values = shap_values[1]
     fig = plt.figure()
 
     shap.summary_plot(
         shap_values,
-        X_transformed,
+        X_sample,
         feature_names=encoding.get_feature_names_out(),
         show=False
     )
@@ -207,7 +229,7 @@ st.markdown("---")
 
 st.markdown(
 """
-Built with **Streamlit + XGBoost + SHAP**
+Built with **Streamlit + Gradient Boosting + SHAP**
 
 """
 )
